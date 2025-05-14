c0f272e69b89c89b7bd74c04f50f486a6fece9e0
Valentine Sierra <valesierracuevas@gmail.com>
2025-05-14 00:12:42 -0400

    Implementación Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/splash_screen.dart';
 import 'pages/main_page.dart';
 
 void main() {
@@ -8,57 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Push button to go to next page :)'),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()
-        {
-          Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MainPage(title: 'Main Page',)),
-              );
-        },
-        tooltip: 'Next',
-        child: const Icon(Icons.arrow_forward_rounded),
-      ),
+      title: 'App Race',
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
     );
   }
 }
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,74 +1,74 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'main_page.dart';
 
-void splashScreen() {
-  runApp(const MyApp());
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
-  static const appTitle = 'Splash Screen';
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+
+    // Iniciar la animación
+    _controller.forward();
+
+    // Navegar al Home después de 3 segundos
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MainPage(title: 'Main Page')),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: appTitle,
-      home: SplashScreen(title: appTitle),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-{
-  @override
-  void initState() 
-  {
-    super.initState();
-    Future.delayed(Duration(seconds: 3), () 
-    {
-      if(context.mounted)
-      {
-        /*Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ProfilePage(title: 'Profile Page',)),
-              );*/
-      }
-    });
-  } 
-
-  @override Widget build(BuildContext context)
-  {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: const Color.fromARGB(255, 243, 226, 230),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Image.asset(
-            //'NAME',
-            //width: 200,
-            //height: 200,
-            //),
-            SizedBox(height: 20),
-            Text(
-              "Proyecto 2",
-              style: GoogleFonts.poppins(
-                color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+        child: FadeTransition(
+          opacity: _animation,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icons/splash_icon.png',
+                width: 200,
+                height: 200,
               ),
-            )
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "Cocina a Tiempo",
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 48, 44, 44),
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
